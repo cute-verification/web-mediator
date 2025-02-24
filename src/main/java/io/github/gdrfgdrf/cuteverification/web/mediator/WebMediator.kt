81d8b14eb76b92379492327569f3684d96f3daa9
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2025-02-24 14:02:14 +0800

    move some codes to WebMediator

--- src/main/java/io/github/gdrfgdrf/cuteverification/web/mediator/WebMediator.kt
+++ src/main/java/io/github/gdrfgdrf/cuteverification/web/mediator/WebMediator.kt
@@ -1,12 +1,16 @@
 package io.github.gdrfgdrf.cuteverification.web.mediator
 
 import io.github.gdrfgdrf.cuteverification.web.mediator.cache.TokenCache
+import io.github.gdrfgdrf.cuteverification.web.mediator.event.composeEvent
+import io.github.gdrfgdrf.cuteverification.web.mediator.event.program.ProgramStartEvent
+import io.github.gdrfgdrf.cuteverification.web.mediator.event.program.ProgramStopEvent
 import io.github.gdrfgdrf.cuteverification.web.mediator.network.RetrofitClient
-import retrofit2.Retrofit
 import java.util.*
 import java.util.logging.Logger
 
 object WebMediator {
+    var started = false
+        private set
     val logger: Logger = Logger.getLogger("CuteVerification-WebMediator")
     private val timerTask = object : TimerTask() {
         override fun run() {
@@ -15,15 +19,33 @@ object WebMediator {
     }
 
     fun start(url: String, username: String, password: String) {
-        RetrofitClient.init(url)
+        if (started) {
+            throw IllegalStateException("The program is already started")
+        }
+        composeEvent(ProgramStartEvent.pre(), ProgramStartEvent.post()) {
+            RetrofitClient.init(url)
 
-        TokenCache.username(username)
-        TokenCache.password(password)
+            TokenCache.username(username)
+            TokenCache.password(password)
 
-        Timer().schedule(timerTask, 0, 2700000L)
+            Timer().schedule(timerTask, 0, 2700000L)
+            started = true
+        }
     }
 
     fun stop() {
-        timerTask.cancel()
+        if (!started) {
+            throw IllegalStateException("The program is not started yet")
+        }
+
+        composeEvent(ProgramStopEvent.pre(), ProgramStopEvent.post()) {
+            timerTask.cancel()
+            TokenCache.username(null)
+            TokenCache.password(null)
+            TokenCache.set(null)
+
+            started = false
+        }
+
     }
 }
